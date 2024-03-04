29df72464fe606061db2f8f705ff5b006e382cee
Nnocj1 <nicholascommey2001@gmail.com>
2024-03-04 12:28:21 +0000

    Week1 learning 1& 2

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -1,7 +1,8 @@
 public static class ArraySelector
 {
     public static void Run()
-    {
+    {    
+        Console.ReadLine();
         var l1 = new[] { 1, 2, 3, 4, 5 };
         var l2 = new[] { 2, 4, 6, 8, 10};
         var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
@@ -11,6 +12,20 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return new int[0];
+        var results = new int[select.Length]; // now i see my mistake. I needed to set it's capacity.
+        var l1Idx = 0;
+        var l2Idx = 0;
+
+        for (var i = 0; i < select.Length; i++)// changing the name from integer to variable is very significant here.
+        {   
+            if (select[i] == 1){
+                results[i] = list1[l1Idx++];
+            }
+
+            else if (select[i] == 2){
+                results[i] = list2[l2Idx++];
+            }
+        }
+        return results;
     }
 }
